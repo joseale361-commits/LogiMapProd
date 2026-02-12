b51ea78f711d65634743113918788251a4b3145f
Jose Ale <joseale361@gmail.com>
2026-02-11 23:50:26 -0500

    Fix config for Next 14

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit.default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
+
+export default withPWA(nextConfig);
